3383bcbada9f5e09ddd16f2b03cddc6b9f5a361d
mobile-apps-developer <apps.mobile.developer@gmail.com>
2020-01-28 21:15:46 +0530

    Refactor code

--- JSOperationTracker/ViewControllers/OperationsViewController.swift
+++ JSOperationTracker/ViewControllers/OperationsViewController.swift
@@ -5,6 +5,7 @@ class OperationsViewController: UIViewController, JSEventsListener {
     // MARK: - IBOutlets
     @IBOutlet var operationsTableView: UITableView!
     @IBOutlet var loader: UIActivityIndicatorView!
+    var progressMaping = [Int:Float]()
 
     // MARK: - Variables
     private var jsOperationManager: JSOperationManager?
@@ -21,7 +22,7 @@ class OperationsViewController: UIViewController, JSEventsListener {
     // MARK: - Private Methods
     private func downloadJSScript() {
         NetworkManager().getData(from: Endpoints.jsScriptEndPoint) { [weak self] data, error in
-            self?.loader.stopAnimating()git 
+            self?.loader.stopAnimating()
             guard let self = self else { return }
             if let _data = data, let jsScript = String(data: _data, encoding: .utf8) {
                 self.evaluteJSScript(jsScript: jsScript)
@@ -46,12 +47,15 @@ class OperationsViewController: UIViewController, JSEventsListener {
 
     // MARK: - JSEventsListener Delegate
     func operationStatus(operation: Operation) {
+        if operation.progress != nil { progressMaping[operation.id] =  operation.progress }
       let index = operation.id - Constants.CELL_INDEX_DECREMENT // JS Operations starting value is 1
       serverOperations[index] = operation
       let indexPath = IndexPath(row: index, section: Constants.ZERO)
       if let _ = operationsTableView.indexPathsForVisibleRows?.contains(indexPath) {
-        operationsTableView.reloadRows(at: [indexPath], with: .none)
-      }
+        let cell = self.operationsTableView.cellForRow(at: IndexPath(item: index, section: 0)) as? OperationCell
+            let tracking = (progressMaping[operation.id] ?? 0.0)
+            cell?.setUpCell(operation: operation, progress: tracking)
+        }
     }
 
     func operationFailed(error: String) {
@@ -68,8 +72,7 @@ extension OperationsViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: Constants.OPERATION_CELL, for: indexPath) as? OperationCell else { return UITableViewCell() }
         let operation = serverOperations[indexPath.row]
-        cell.setUpCell(operation: operation)
-        
+        cell.setUpCell(operation: operation, progress: progressMaping[operation.id] ?? 0.0)
         return cell
     }
 }
--- JSOperationTracker/Views/Cells/OperationCell.swift
+++ JSOperationTracker/Views/Cells/OperationCell.swift
@@ -6,24 +6,23 @@ class OperationCell: UITableViewCell {
     @IBOutlet var operationTitle: UILabel!
     @IBOutlet weak var progressRing: UICircularProgressRing!
     
-    func setUpCell(operation: Operation) {
+    func setUpCell(operation: Operation, progress: Float) {
         self.operationTitle.text = "Operation " + "\(operation.id)"
-        if let progress = operation.progress {
-            print(CGFloat(progress * 100.0))
-            self.progressRing.startProgress(to: CGFloat(progress * 100.0), duration: 0)
-            self.progressRing.font = UIFont.systemFont(ofSize: 12)
-            if let state = operation.state {
-                self.statusLabel.text = state
+        self.progressRing.font = UIFont.systemFont(ofSize: 12)
 
-                if state == OperationUIStatus.ERROR {
-                    self.progressRing.innerRingColor = .systemPurple
-                }
-                if state == OperationUIStatus.COMPLETED {
-                    self.progressRing.innerRingColor = .systemGreen
-                }
-                if state == OperationUIStatus.IN_PROGRESS {
-                    self.progressRing.innerRingColor = .systemIndigo
-                }
+        self.progressRing.startProgress(to: CGFloat(progress * 100.0), duration: 0)
+
+        if let state = operation.state {
+            self.statusLabel.text = state
+
+            if state == OperationUIStatus.ERROR {
+                self.progressRing.innerRingColor = .systemPurple
+            }
+            if state == OperationUIStatus.COMPLETED {
+                self.progressRing.innerRingColor = .systemGreen
+            }
+            if state == OperationUIStatus.IN_PROGRESS {
+                self.progressRing.innerRingColor = .systemIndigo
             }
         }
     }
